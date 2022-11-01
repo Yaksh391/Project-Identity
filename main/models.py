1be855a2c6a16daa3ab0a2f04208d08769d2b2ca
Yaksh <s.yaksh.patel@fountainheadschools.org>
2022-11-01 16:42:17 +0530

    added the personal information model as Person

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30, default="")
+    middle_name = models.CharField(max_length=30, default="")
+    last_name = models.CharField(max_length=30, default="")
+    picture = models.ImageField(upload_to='uploads/')
+    picture_background_removed = models.ImageField(upload_to='uploads/', default="")
+    hobbies = models.TextField(default="", help_text="Please separate them by a comma.")
+    interests = models.TextField(default="", help_text="Please separate them by a comma.")
+    skills = models.TextField(default="", help_text="Please separate them by a comma.")
