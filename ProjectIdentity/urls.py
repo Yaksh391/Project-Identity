a20e71fd146acaa0af4c7896399bfb4a062a03eb
Yaksh <s.yaksh.patel@fountainheadschools.org>
2022-11-02 15:40:59 +0530

    added projects url path for project showcase html

--- ProjectIdentity/urls.py
+++ ProjectIdentity/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainViews.Home),
+    path('projects/', mainViews.Project_Showcase)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
