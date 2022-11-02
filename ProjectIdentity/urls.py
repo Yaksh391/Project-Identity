3dab0337dce65c08e4b25f92b2cd63ed6ec67bc1
Yaksh <s.yaksh.patel@fountainheadschools.org>
2022-11-02 16:26:59 +0530

    added projects path and also added a searchable projects preview path based on title

--- ProjectIdentity/urls.py
+++ ProjectIdentity/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainViews.Home),
-    path('projects/', mainViews.Project_Showcase)
+    path('projects/', mainViews.Project_Showcase),
+    path('projects/<str:title>/', mainViews.Project_Preview),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
