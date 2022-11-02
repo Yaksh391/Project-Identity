17bbf8306e99a16bf13c13fd5d1049cfb82cd22f
Yaksh <s.yaksh.patel@fountainheadschools.org>
2022-11-02 15:39:03 +0530

    changed model to obj in Home function. Added function for showing project showcase html

--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Person
+from .models import Project
 # Create your views here.
 
 
 def Home(request, *args, **kwargs):
-    model = Person.objects.filter(first_name__exact="Yaksh")
+    obj = Person.objects.filter(first_name__exact="Yaksh")
     # model = Person.objects.all()
-    context = {"person": model[0]}
+    context = {"person": obj[0]}
     return render(request, 'home.html', context)
+
+
+def Project_Showcase(request, *args, **kwargs):
+    objs = Project.objects.all()
+    context = {"projects": objs}
+    return render(request, 'project/project_showcase.html', context)
