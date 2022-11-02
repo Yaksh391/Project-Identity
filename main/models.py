bfeea96de9b3b7f3a6a81adcf2f7575e0687401e
Yaksh <s.yaksh.patel@fountainheadschools.org>
2022-11-02 13:19:54 +0530

    added project model and tried to use dynamic uploading system

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 
 # Create your models here.
 
@@ -13,3 +13,24 @@ class Person(models.Model):
     hobbies = models.TextField(default="", help_text="Please separate them by a comma.")
     interests = models.TextField(default="", help_text="Please separate them by a comma.")
     skills = models.TextField(default="", help_text="Please separate them by a comma.")
+
+
+def upload_path_project(instance, filename):
+    return os.path.join(instance, filename)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=50, default="")
+    description = models.TextField(max_length=500, default="")
+    references = models.TextField(max_length=1000, default="")
+    cover_image = models.ImageField(upload_to=upload_path_project, default="")
+    image_1 = models.ImageField(upload_to=upload_path_project, default="")
+    image_2 = models.ImageField(upload_to=upload_path_project, default="")
+    image_3 = models.ImageField(upload_to=upload_path_project, default="")
+    image_4 = models.ImageField(upload_to=upload_path_project, default="")
+    image_5 = models.ImageField(upload_to=upload_path_project, default="")
+    image_6 = models.ImageField(upload_to=upload_path_project, default="")
+    image_7 = models.ImageField(upload_to=upload_path_project, default="")
+    youtube_video = models.CharField(default="", max_length=40)
+    discipline_of_work = models.TextField(default="", help_text="Please separate them by a comma.")
+
