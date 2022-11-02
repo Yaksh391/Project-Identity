fa067699ca05005ebafeacbfd14cb2214bf7d358
Yaksh <s.yaksh.patel@fountainheadschools.org>
2022-11-02 13:45:32 +0530

    fixed upload directory for project model

--- main/models.py
+++ main/models.py
@@ -15,22 +15,22 @@ class Person(models.Model):
     skills = models.TextField(default="", help_text="Please separate them by a comma.")
 
 
-def upload_path_project(instance, filename):
-    return os.path.join(instance, filename)
+def path_project_title(instance, filename):
+    return 'projectInformation/{0}/{1}'.format(instance.title, filename)
 
 
 class Project(models.Model):
     title = models.CharField(max_length=50, default="")
     description = models.TextField(max_length=500, default="")
     references = models.TextField(max_length=1000, default="")
-    cover_image = models.ImageField(upload_to=upload_path_project, default="")
-    image_1 = models.ImageField(upload_to=upload_path_project, default="")
-    image_2 = models.ImageField(upload_to=upload_path_project, default="")
-    image_3 = models.ImageField(upload_to=upload_path_project, default="")
-    image_4 = models.ImageField(upload_to=upload_path_project, default="")
-    image_5 = models.ImageField(upload_to=upload_path_project, default="")
-    image_6 = models.ImageField(upload_to=upload_path_project, default="")
-    image_7 = models.ImageField(upload_to=upload_path_project, default="")
+    cover_image = models.ImageField(upload_to=path_project_title, default="")
+    image_1 = models.ImageField(upload_to=path_project_title, default="")
+    image_2 = models.ImageField(upload_to=path_project_title, default="")
+    image_3 = models.ImageField(upload_to=path_project_title, default="")
+    image_4 = models.ImageField(upload_to=path_project_title, default="")
+    image_5 = models.ImageField(upload_to=path_project_title, default="")
+    image_6 = models.ImageField(upload_to=path_project_title, default="")
+    image_7 = models.ImageField(upload_to=path_project_title, default="")
     youtube_video = models.CharField(default="", max_length=40)
     discipline_of_work = models.TextField(default="", help_text="Please separate them by a comma.")
 
