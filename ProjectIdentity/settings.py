b53535b3c3052c15dde181df159c4b809c0d9b7e
Yaksh <s.yaksh.patel@fountainheadschools.org>
2023-04-06 22:15:15 +0530

    removed filer app

--- ProjectIdentity/settings.py
+++ ProjectIdentity/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'easy_thumbnails',
-    'filer',
-    'mptt',
+    # 'easy_thumbnails',
+    # 'filer',
+    # 'mptt',
 ]
 
 MIDDLEWARE = [
